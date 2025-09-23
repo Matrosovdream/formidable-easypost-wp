a4982f550d1b601817f5d2f5ce992bf7924e9240
Stan Matrosov <matrosovdream@gmail.com>
2025-09-23 13:28:59 +0200

    Shipment Model, new method shipmentStatuses()

--- classes/models/FrmEasypostShipmentModel.php
+++ classes/models/FrmEasypostShipmentModel.php
@@ -255,4 +255,18 @@ class FrmEasypostShipmentModel extends FrmEasypostAbstractModel {
         return $this->multipleUpdateCreateAbstract( $rows, $cols, $formats, $uniqueKey = 'easypost_id' );
     }
 
+    public function shipmentStatuses(): array {
+        return [
+            'unknown'      => __( 'Unknown', 'easypost-wp' ),
+            'pre_transit'  => __( 'Pre-Transit', 'easypost-wp' ),
+            'in_transit'   => __( 'In Transit', 'easypost-wp' ),
+            'out_for_delivery' => __( 'Out for Delivery', 'easypost-wp' ),
+            'delivered'    => __( 'Delivered', 'easypost-wp' ),
+            'return_to_sender' => __( 'Return to Sender', 'easypost-wp' ),
+            'available_for_pickup' => __( 'Available for Pickup', 'easypost-wp' ),
+            'failure'      => __( 'Failure', 'easypost-wp' ),
+            'cancelled'    => __( 'Cancelled', 'easypost-wp' ),
+        ];  
+    }    
+
 }
