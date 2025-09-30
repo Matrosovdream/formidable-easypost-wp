5ace390f95b6a88fcecbb11d709c78f06c657a4f
Stan Matrosov <matrosovdream@gmail.com>
2025-09-30 17:25:14 +0200

    Action frm_after_create_update_entry, removed function of updating entry status

--- actions/admin/frm_after_create_update_entry.php
+++ actions/admin/frm_after_create_update_entry.php
@@ -2,23 +2,16 @@
 
 /** Hook: after create */
 add_action('frm_after_create_entry', function (int $entry_id, int $form_id) {
-
-    // Update entry status based on field conditions
-    $applyForm = new FrmUpdateApplyForm();
-    $applyForm->updateEntryStatus($entry_id);
     
     // Save PDF of entry on the server
     $entryPdf = new FrmSaveEntryPdf();
     $entryPdf->saveEntryPdf( $entry_id );
 
     
-}, 20, 2);
+}, 5, 2);
 
 /** Hook: after update */
 add_action('frm_after_update_entry', function (int $entry_id, int $form_id) {
 
-    // Update entry status based on field conditions
-    $applyForm = new FrmUpdateApplyForm();
-    $applyForm->updateEntryStatus($entry_id);
 
-}, 20, 2);
+}, 5, 2);
