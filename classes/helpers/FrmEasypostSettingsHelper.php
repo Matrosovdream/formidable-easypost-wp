e1ec2623f7d44089f24b5ca13b6eab87652d7a94
Stan Matrosov <matrosovdream@gmail.com>
2025-09-30 19:29:19 +0200

    Settings helper, new method for Voiding shipment params

--- classes/helpers/FrmEasypostSettingsHelper.php
+++ classes/helpers/FrmEasypostSettingsHelper.php
@@ -20,6 +20,14 @@ class FrmEasypostSettingsHelper {
         return $settings['usps_timezone'] ?? null;
     }
 
+    public function getVoidShipment(): array {
+        $opts = $this->getAllSettings();
+        return [
+            'void_statuses'   => isset($opts['void_statuses']) && is_array($opts['void_statuses']) ? $opts['void_statuses'] : [],
+            'void_after_days' => max(0, (int)($opts['void_after_days'] ?? 0)),
+        ];
+    }
+
     public function getAllSettings() {
         return get_option('frm_easypost', []);
     }
