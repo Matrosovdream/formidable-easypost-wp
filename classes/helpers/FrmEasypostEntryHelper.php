7c7f3aefc6b259a6d351bc0307185026f14dfb02
Stan Matrosov <matrosovdream@gmail.com>
2026-01-05 20:16:04 +0700

    FrmEasypostEntryhelper, getEntryAddressFields() - injected const from References

--- classes/helpers/FrmEasypostEntryHelper.php
+++ classes/helpers/FrmEasypostEntryHelper.php
@@ -54,16 +54,8 @@ class FrmEasypostEntryHelper {
 
     protected function getEntryAddressFields( int $entry_id ) {
         
-        $fields = [
-            'firstname' => 1,
-            'lastname'  => 2,
-            'street1'  => 37,
-            'street2'  => 38,
-            'city'      => 39,
-            'state'     => 40,
-            'zip'       => 41,
-            'phone'     => 64,
-        ];
+        // From /references.php
+        $fields = FRM_EP_ENTRY_ADDRESS_FIELDS ?? [];
 
         $entry = \FrmEntry::getOne($entry_id, true);
         if (!$entry) {
