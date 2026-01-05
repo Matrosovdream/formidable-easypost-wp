38d278daebc0aebf39a1ffec508f0a2624d1a87f
Stan Matrosov <matrosovdream@gmail.com>
2026-01-05 20:16:18 +0700

    References file, initial version

---
+++ references.php
@@ -0,0 +1,12 @@
+<?php
+
+const FRM_EP_ENTRY_ADDRESS_FIELDS = [
+    'firstname' => 1,
+    'lastname'  => 2,
+    'street1'  => 37,
+    'street2'  => 38,
+    'city'      => 39,
+    'state'     => 40,
+    'zip'       => 41,
+    'phone'     => 248,
+];
